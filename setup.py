6b25cc499cb32e3f2ee3801dc6bc803bea950bcf
Sam Sandberg <sam.sandberg@gmail.com>
2015-07-10 17:20:30 -0400

    Updated `setup.py`, gearing up for pypi push

--- setup.py
+++ setup.py
@@ -3,12 +3,15 @@ from setuptools import setup
 
 
 setup(
-    name="pyslack",
-    version="0.5.1",
-    description="A Python wrapper for Slack's API",
     author="@LoisaidaSam",
     author_email="sam.sandberg@gmail.com",
-    packages=["pyslack"],
+    description="A Python wrapper for Slack's API",
     install_requires=["requests"],
+    keywords="slack api wrapper",
+    license="MIT",
+    name="pyslack-real",
+    packages=["pyslack"],
     test_suite="tests",
+    url="https://github.com/loisaidasam/pyslack",
+    version="0.5.2",
 )
